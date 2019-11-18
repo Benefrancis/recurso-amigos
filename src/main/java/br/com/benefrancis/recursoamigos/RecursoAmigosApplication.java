0b8f78217324cff07434e57c870b37edb740e5d4
Benefrancis do Nascimento (PRR3) <benefrancis@mpf.mp.br>
2019-11-18 16:24:32 -0200

    Commit inicial

---
+++ src/main/java/br/com/benefrancis/recursoamigos/RecursoAmigosApplication.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.recursoamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursoAmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursoAmigosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/benefrancis/recursoamigos/RecursoAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.recursoamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursoAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
