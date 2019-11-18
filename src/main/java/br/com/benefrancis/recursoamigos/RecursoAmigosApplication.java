8e8290db1d5096fe2dcc709d79993a645cfecb3f
Benefrancis do Nascimento (PRR3) <benefrancis@mpf.mp.br>
2019-11-18 18:24:25 -0200

    Quando adiciono a anotação: @EnableResourceServer, o Spring Security precisa da informação do security oauth2 resource token-info-uri e das informações do client client-id e client-secret

--- src/main/java/br/com/benefrancis/recursoamigos/RecursoAmigosApplication.java
+++ src/main/java/br/com/benefrancis/recursoamigos/RecursoAmigosApplication.java
@@ -2,8 +2,10 @@ package br.com.benefrancis.recursoamigos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class RecursoAmigosApplication {
 
 	public static void main(String[] args) {
