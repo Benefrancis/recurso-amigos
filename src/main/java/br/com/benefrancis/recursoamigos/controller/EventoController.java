63c057d52535c0150dfc00706d6c939e92105736
Benefrancis do Nascimento (PRR3) <benefrancis@mpf.mp.br>
2019-11-18 20:31:21 -0200

    Só poderá salvar evento se tiver o devido papel

--- src/main/java/br/com/benefrancis/recursoamigos/controller/EventoController.java
+++ src/main/java/br/com/benefrancis/recursoamigos/controller/EventoController.java
@@ -3,6 +3,7 @@ package br.com.benefrancis.recursoamigos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ public class EventoController {
 	}
 
 	@PostMapping(value = "/evento")
+	@PreAuthorize("hasAuthority('create_evento')")
 	public Evento save(@RequestBody Evento e) {
 		Evento salvo = service.save(e);
 		return findById(salvo.getId());
